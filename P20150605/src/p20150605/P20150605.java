a2454cb859acaf24308f3cac0103e4f3aa2dd221
irimar <irinam5592@gmail.com>
2015-06-05 12:29:07 +0200

    Tras documentar javadoc Irina Markova

---
+++ P20150605/src/p20150605/P20150605.java
@@ -0,0 +1,16 @@
+package p20150605;
+
+public class P20150605 {
+/**
+ * Gestiona las cuñas de anuncios al introducir.
+ * 
+ * @author Irina Markova
+ * @version 0.0.1
+ * @see AudioSpot
+ */
+    public static void main(String[] args)
+    {
+        //
+    }
+    
+}
